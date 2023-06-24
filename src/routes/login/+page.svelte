6cd746038cc4a67a0a30a28845dc640f42ca4b5b
Dlurak <noreply@noreply.noreply>
2023-06-24 14:42:49 +0200

    removed unused css

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -114,45 +114,6 @@
         margin-top: 0;
     }
 
-    #login > button {
-        width: 100%;
-        background-color: var(--submit-button-background-color);
-        color: var(--text-color);
-
-        padding-left: 20px;
-        padding-right: 20px;
-        padding-top: 10px;
-        padding-bottom: 10px;
-
-        border-radius: 10px;
-        border: none;
-
-        margin-top: 20px;
-
-        font-weight: 600;
-
-        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
-
-        transition: all var(--animation);
-    }
-    #login > button:not(:disabled):hover,
-    #login > button:not(:disabled):focus {
-        cursor: pointer;
-        transform: scale(1.05);
-        box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.6);
-        outline: none;
-    }
-    #login > button:not(:disabled):active {
-        cursor: grabbing;
-        transform: scale(1);
-        box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.8);
-    }
-
-    #login > button:disabled {
-        cursor: not-allowed;
-        opacity: 0.5;
-    }
-
     a {
         color: var(--link-color);
     }
