3c788e3356c2bacfc564d0f1814d860b7d9c80fb
Dlurak <noreply@noreply.noreply>
2023-07-04 17:46:18 +0200

    added a text to inform users that there are no requests

--- src/routes/rideRequests/+page.svelte
+++ src/routes/rideRequests/+page.svelte
@@ -1,14 +1,50 @@
-<script>
+<script lang="ts">
+    import { onMount } from "svelte";
     import Map from "../../components/Map.svelte";
+
+    let requests: Object[] = [];
+
+    onMount(async () => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            window.location.href = "/login";
+        }
+        const configResponse = await fetch("config.json", {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        });
+        const config = await configResponse.json();
+
+        const url = config.apiUrl + "/rideRequest";
+
+        const response = await fetch(url, {
+            method: "GET",
+            // authorize with the jwt token stored in the local storage
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            }
+        });
+
+        requests = await response.json();
+    });
 </script>
 
 <main>
-    <ul>
-        <li>
-            This will get dynamically populated with the ride requests in the near future
-        </li>
-    </ul>
-    <div>
+    {#if requests}
+        <div id="notFound">
+            <p>There are no ride requests as for now</p>
+        </div>
+    {:else}
+        <ul>
+            <li>
+                This will get dynamically populated with the ride requests in the near future
+            </li>
+        </ul>
+    {/if}
+    <div id="mapContainer">
         <Map />
     </div>
 </main>
@@ -21,14 +57,33 @@
         flex-wrap: nowrap;
         height: 100vh;
     }
-    main > div {
+    main > #mapContainer {
         flex: 3;
     }
-    main > ul {
+    main > ul,
+    #notFound {
         list-style-type: none;
         margin: 0;
         padding: 0;
         flex: 1;
+    }
+
+    main * {
+        height: 100%;
+    }
+
+    #notFound {
+        display: flex;
+        justify-content: center;
+    }
+    #notFound > p {
+        display: inline;
+        margin: 0;
+        padding: 0;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 
     @media only screen and (max-width: 600px) {
@@ -36,11 +91,12 @@
             flex-direction: column-reverse;
             height: max-content;
         }
-        main > div {
+        main > #mapContainer {
             flex: 1;
             aspect-ratio: 1/1.25;
         }
-        main > ul {
+        main > ul,
+        #notFound {
             flex: 1;
             height: fit-content;
         }
